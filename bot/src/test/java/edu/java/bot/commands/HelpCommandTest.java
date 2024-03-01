fea8030d62a6165324c09fc353daf999865fcb54
LARCHIK <ovchinnikov20035696@gmail.com>
2024-03-01 19:04:29 +0500

    Fixed test

--- bot/src/test/java/edu/java/bot/commands/HelpCommandTest.java
+++ bot/src/test/java/edu/java/bot/commands/HelpCommandTest.java
@@ -4,22 +4,19 @@ import com.pengrad.telegrambot.model.Chat;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
+import edu.java.bot.BotApplication;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest(classes = {BotApplication.class})
 public class HelpCommandTest {
-
-    // Class to be tested
-    @InjectMocks
-    private HelpCommand helpCommand;
 
     // Dependencies (will be mocked)
     @Mock
@@ -30,6 +27,12 @@ public class HelpCommandTest {
     private Chat chat;
 
     final long chatId = 123L;
+
+    @Autowired
+    private List<Command> listOfCommand;
+
+    @Autowired
+    private HelpCommand helpCommand;
 
     @BeforeEach
     void init() {
@@ -50,11 +53,11 @@ public class HelpCommandTest {
 
     private String expectedResultBuilder() {
         StringBuilder message = new StringBuilder("Available commands:\n");
-
-        message.append("/start - Register the user\n");
-        message.append("/list - Show the list of tracked links\n");
-        message.append("/track - Start link tracking\n");
-        message.append("/untrack - Stop link tracking\n");
+        for (Command command : listOfCommand) {
+            if (!command.command().equals("/help")) {
+                message.append(command.command()).append(" - ").append(command.description()).append("\n");
+            }
+        }
         return message.toString();
     }
 }
