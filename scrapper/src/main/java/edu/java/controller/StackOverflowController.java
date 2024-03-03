411a0fe4ac67ae8446288f189bc6e5bb2f921c32
LARCHIK <ovchinnikov20035696@gmail.com>
2024-03-03 17:49:34 +0500

    Implement BotClient

---
+++ scrapper/src/main/java/edu/java/controller/StackOverflowController.java
@@ -0,0 +1,26 @@
+package edu.java.controller;
+
+import edu.java.client.StackOverflowClient;
+import edu.java.entity.dto.StackOverflowResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stackoverflow")
+public class StackOverflowController {
+
+    private final StackOverflowClient stackOverflowClient;
+
+    @Autowired
+    public StackOverflowController(StackOverflowClient stackOverflowClient) {
+        this.stackOverflowClient = stackOverflowClient;
+    }
+
+    @GetMapping("/question/{questionId}")
+    public StackOverflowResponse getQuestion(@PathVariable long questionId) {
+        return stackOverflowClient.fetchQuestion(questionId);
+    }
+}
